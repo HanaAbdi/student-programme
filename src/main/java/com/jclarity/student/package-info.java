01ec116c8eddd58d35a2278288c5b9860a540e47
Martijn Verburg <martijnverburg@gmail.com>
2014-07-06 19:56:45 +0100

    Initial project

---
+++ src/main/java/com/jclarity/student/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The top level package for the student programme
+ */
+package com.jclarity.student;
---
+++ src/test/java/com/jclarity/student/AppTest.java
@@ -0,0 +1,24 @@
+package com.jclarity.student;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+    /**
+     * You can use the suppress warnings annotation if you are absolutely sure the IDE
+     * is reporting a false positive.
+     */
+    @SuppressWarnings("static-method")
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
